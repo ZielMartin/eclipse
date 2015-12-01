9f54720304aa782033d78e9f61d5c5a38fea728a
ZielMartin <Ziel.Martin@gmail.com>
2015-12-01 08:41:46 +0100

    UML

--- Uebungen/src/kw49/verwaltungMitarbeiter/Main.java
+++ Uebungen/src/kw49/verwaltungMitarbeiter/Main.java
@@ -29,6 +29,8 @@ public class Main {
 		
 		System.out.println("Counter: "+Person.getCounter());
 		System.out.println((verbAng.getClass() + "").substring(33));
+		if(studiHiwi instanceof Mitarbeiter) System.out.println("Ja");
+		else System.out.println("Nein");
 	}
 
 }
