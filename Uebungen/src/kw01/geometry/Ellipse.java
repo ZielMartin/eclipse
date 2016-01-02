8ddb26f3b215bd4327ea86771f2b24b2ea48a1bc
ZielMartin <Ziel.Martin@gmail.com>
2016-01-02 17:52:03 +0100

    compareTo von Rectangle&Ellipse zu Primitive geschoben, da redundant

--- Uebungen/src/kw01/geometry/Ellipse.java
+++ Uebungen/src/kw01/geometry/Ellipse.java
@@ -1,31 +1,31 @@
 package kw01.geometry;
 
-public class Ellipse extends Primitive implements IRound{
+public class Ellipse extends Primitive implements IRound {
 	private Point center;
 	private int longSemiaxis;
 	private int shortSemiaxis;
-	
+
 	public Ellipse(Point center, int longSemiaxis, int shortSemiaxis) {
 		super();
 		this.center = center;
 		this.longSemiaxis = longSemiaxis;
 		this.shortSemiaxis = shortSemiaxis;
 	}
-	
+
 	@Override
 	public double getCircumference() {
-		return Math.PI*(longSemiaxis+shortSemiaxis);
+		return Math.PI * (longSemiaxis + shortSemiaxis);
 	}
 
 	@Override
 	public double getArea() {
-		return Math.PI*longSemiaxis*shortSemiaxis;
+		return Math.PI * longSemiaxis * shortSemiaxis;
 	}
-	
+
 	@Override
 	public String toString() {
-		String s=super.toString();
-		return s+";"+this.getCenter()+";"+this.getLongSemiaxis()+";"+this.getShortSemiaxis();
+		String s = super.toString();
+		return s + ";" + this.getCenter() + ";" + this.getLongSemiaxis() + ";" + this.getShortSemiaxis();
 	}
 
 	public Point getCenter() {
@@ -38,11 +38,5 @@ public class Ellipse extends Primitive implements IRound{
 
 	public int getShortSemiaxis() {
 		return shortSemiaxis;
-	}
-
-	@Override
-	public int compareTo(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 }
--- Uebungen/src/kw01/geometry/Primitive.java
+++ Uebungen/src/kw01/geometry/Primitive.java
@@ -1,11 +1,19 @@
 package kw01.geometry;
 
-public abstract class Primitive<T> implements Comparable<T>{
-	
+public abstract class Primitive<T> implements Comparable<T> {
+
 	public abstract double getCircumference();
+
 	public abstract double getArea();
-	
+
 	public String toString() {
-		return"Typ: "+getClass()+";"+"area: "+String.format("%4.2f", getArea())+";"+"circumference: "+String.format("%4.2f",getCircumference());
+		return "Typ: " + getClass() + ";" + "area: " + String.format("%4.2f", getArea()) + ";" + "circumference: "
+				+ String.format("%4.2f", getCircumference());
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
